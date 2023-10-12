6ac9b2261bb218f7d70247d62ed763f3cbfed439
MatheusFreire7 <cc21251@g.unicamp.br>
2023-10-12 20:51:42 -0300

    first commit

---
+++ src/main/java/br/com/matheusfreire/todolist/user/IUserRepository.java
@@ -0,0 +1,7 @@
+package br.com.matheusfreire.todolist.user;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID>{
+    UserModel findByUsername(String username);
+}
