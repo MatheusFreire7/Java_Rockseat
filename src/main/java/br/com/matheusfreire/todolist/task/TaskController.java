a11b38405c8434462b8317bf1efed48af9499280
MatheusFreire7 <cc21251@g.unicamp.br>
2023-10-13 12:35:07 -0300

    DockerFile deploy

--- src/main/java/br/com/matheusfreire/todolist/task/TaskController.java
+++ src/main/java/br/com/matheusfreire/todolist/task/TaskController.java
@@ -50,13 +50,21 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
-    public TaskModel update(@RequestBody TaskModel taskModel,HttpServletRequest request, @PathVariable UUID id ){
-         var idUser = request.getAttribute("idUser");
-
+    public ResponseEntity update(@RequestBody TaskModel taskModel,HttpServletRequest request, @PathVariable UUID id ){
          var task = this.taskRepository.findById(id).orElse(null);
 
-         Utils.copyNonNullProperties(taskModel, task);
+         if(task == null){
+             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa Não Encontrada!");
+         }
 
-         return this.taskRepository.save(task);
+         var idUser = request.getAttribute("idUser");
+
+         if(!task.getIdUser().equals(idUser)){
+              return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para Alterar esta Tarefa");
+         }
+
+         Utils.copyNonNullProperties(taskModel, task);
+         var taskUpdated = this.taskRepository.save(task);
+         return ResponseEntity.status(HttpStatus.OK).body(taskUpdated);
     }
 }
--- src/main/java/br/com/matheusfreire/todolist/task/TaskModel.java
+++ src/main/java/br/com/matheusfreire/todolist/task/TaskModel.java
@@ -29,5 +29,12 @@ public class TaskModel {
     @CreationTimestamp
     private LocalDateTime createdAt;
 
+    public void setTitle(String title) throws Exception{
+        if(title.length() > 50){
+            throw new Exception("o campo Title deve conter no máximo 50 Caracteres");
+        }
+        this.title = title;
+    }
+
 }
 
